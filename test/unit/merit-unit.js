4533ac75fffd6584c4af1bff182278a1f1f8e71c
Chris Troutner <chris.troutner@gmail.com>
2021-01-10 18:17:51 -0800

    feat(parent age): Added methods for retrieving parent UTXO age

--- test/unit/merit-unit.js
+++ test/unit/merit-unit.js
@@ -189,12 +189,84 @@ describe('#merit.js', () => {
 
     it('should handle errors', async () => {
       try {
-        // Force and error.
+        // Force an error.
         sandbox.stub(uut, 'getTokenUtxos').rejects(new Error('test error'))
 
         await uut.agMerit()
 
         assert.equal(true, false, 'Unexpected result')
+      } catch (err) {
+        // console.log(err)
+        assert.include(err.message, 'test error')
+      }
+    })
+  })
+
+  describe('#findTokenParent', () => {
+    it('should get the parent of a token UTXO', async () => {
+      // Mock network calls.
+      sandbox.stub(uut.bchjs.RawTransactions, 'getRawTransaction').resolves(mockData.mockTxInputs)
+      sandbox.stub(uut.bchjs.Electrumx, 'transactions').resolves(mockData.mockTxHistory)
+      sandbox.stub(uut.bchjs.SLP.Utils, 'hydrateUtxos').resolves(mockData.mockParentUtxo1)
+
+      const addr = 'bitcoincash:qzjgc7cz99hyh98yp4y6z5j40uwnd78fw5lx2m4k9t'
+      const txid =
+        '548198c66640b14c4c175ba5f88d73c63b00f65bc3adc3ea2e94fc41919c6c75'
+
+      const parentUtxo = await uut.findTokenParent(txid, addr)
+      // console.log(`parentUtxo: ${JSON.stringify(parentUtxo, null, 2)}`)
+
+      assert.equal(parentUtxo.tx_hash, 'b7b28a03575bae28c421306fe6727d26c8a6c109b03dfdd276e7bfe32d83e850')
+      assert.equal(parentUtxo.isValid, true)
+    })
+
+    it('should handle errors', async () => {
+      try {
+        // Force an error.
+        sandbox.stub(uut.bchjs.RawTransactions, 'getRawTransaction').rejects(new Error('test error'))
+
+        const addr = 'bitcoincash:qzjgc7cz99hyh98yp4y6z5j40uwnd78fw5lx2m4k9t'
+        const txid =
+          '548198c66640b14c4c175ba5f88d73c63b00f65bc3adc3ea2e94fc41919c6c75'
+
+        await uut.findTokenParent(txid, addr)
+
+        assert.fail('Unexpected result')
+      } catch (err) {
+        // console.log(err)
+        assert.include(err.message, 'test error')
+      }
+    })
+  })
+
+  describe('#getParentAge', () => {
+    it('should get the age of the oldest parent', async () => {
+      // Mock network calls.
+      sandbox.stub(uut, 'findTokenParent').onCall(0).resolves(mockData.mockParentUtxo2).onCall(1).resolves(false)
+
+      const addr = 'bitcoincash:qqlrzp23w08434twmvr4fxw672whkjy0py26r63g3d'
+      const txid =
+        'b79a8c9522ad707275f1fcc7fcd07affe0a4c6dd4abb20d0dac8c7b3320f4002'
+
+      const parentUtxo = await uut.getParentAge(txid, addr)
+      // console.log(`parentUtxo: ${JSON.stringify(parentUtxo, null, 2)}`)
+
+      assert.equal(parentUtxo.tx_hash, 'a5f3e9a08b0f592040b7538d0bf95b646c9e416bec0f909f81da6518ba32928f')
+      assert.equal(parentUtxo.isValid, true)
+    })
+
+    it('should handle errors', async () => {
+      try {
+        // Force an error.
+        sandbox.stub(uut, 'findTokenParent').rejects(new Error('test error'))
+
+        const addr = 'bitcoincash:qzjgc7cz99hyh98yp4y6z5j40uwnd78fw5lx2m4k9t'
+        const txid =
+          '548198c66640b14c4c175ba5f88d73c63b00f65bc3adc3ea2e94fc41919c6c75'
+
+        await uut.getParentAge(txid, addr)
+
+        assert.fail('Unexpected result')
       } catch (err) {
         // console.log(err)
         assert.include(err.message, 'test error')
--- test/unit/mocks/merit-mocks.js
+++ test/unit/mocks/merit-mocks.js
@@ -204,9 +204,115 @@ const meritHydratedUtxos = [
   }
 ]
 
+const mockTxInputs = {
+  txid: '548198c66640b14c4c175ba5f88d73c63b00f65bc3adc3ea2e94fc41919c6c75',
+  hash: '548198c66640b14c4c175ba5f88d73c63b00f65bc3adc3ea2e94fc41919c6c75',
+  version: 2,
+  size: 660,
+  locktime: 0,
+  vin: [
+    {
+      txid: 'b7b28a03575bae28c421306fe6727d26c8a6c109b03dfdd276e7bfe32d83e850',
+      vout: 2,
+      scriptSig: {
+        asm: '304402202598a38f7aa4257c7444a1047e5709b1d518961691f22431c7c1d90062c28ccc02204d47d662be0e06930f78ce78eee06ca55d8bac0ce8a1073e6f9e6ce48f4c36bb[ALL|FORKID] 033a24d13b45eaf53bebc7da5b7ee79a39615790b4fb16dab048fdcc5abd3764ef',
+        hex: '47304402202598a38f7aa4257c7444a1047e5709b1d518961691f22431c7c1d90062c28ccc02204d47d662be0e06930f78ce78eee06ca55d8bac0ce8a1073e6f9e6ce48f4c36bb4121033a24d13b45eaf53bebc7da5b7ee79a39615790b4fb16dab048fdcc5abd3764ef'
+      },
+      sequence: 4294967295
+    },
+    {
+      txid: '5ed4f4d8a02693048975a98c59c7b8b04b2c0656b080595826f043728df46edc',
+      vout: 1,
+      scriptSig: {
+        asm: '3044022037c16bb5d36adf8abb74a963a093cf5212ed487b0214be37a90dbb127f14bf2f0220133bca5460fa7b2364adf7210a431c42f0cef61f2e2685e70fce4194ca14b73a[ALL|FORKID] 033a24d13b45eaf53bebc7da5b7ee79a39615790b4fb16dab048fdcc5abd3764ef',
+        hex: '473044022037c16bb5d36adf8abb74a963a093cf5212ed487b0214be37a90dbb127f14bf2f0220133bca5460fa7b2364adf7210a431c42f0cef61f2e2685e70fce4194ca14b73a4121033a24d13b45eaf53bebc7da5b7ee79a39615790b4fb16dab048fdcc5abd3764ef'
+      },
+      sequence: 4294967295
+    },
+    {
+      txid: 'b94dee98eef2fd8e946b57c559e16ea737d2344cd0f1fbcf550be86b4b3f1f0c',
+      vout: 2,
+      scriptSig: {
+        asm: '304402201f6545811bb74104a4582522fa5a1ccb17fed9f94bba7a91a629575f217b2ecd02206035ebba70b9d01b440739ecbe97a9a70666e14fb051d5adc4dcad93221317b4[ALL|FORKID] 033a24d13b45eaf53bebc7da5b7ee79a39615790b4fb16dab048fdcc5abd3764ef',
+        hex: '47304402201f6545811bb74104a4582522fa5a1ccb17fed9f94bba7a91a629575f217b2ecd02206035ebba70b9d01b440739ecbe97a9a70666e14fb051d5adc4dcad93221317b44121033a24d13b45eaf53bebc7da5b7ee79a39615790b4fb16dab048fdcc5abd3764ef'
+      },
+      sequence: 4294967295
+    }
+  ],
+  vout: []
+}
+
+const mockTxHistory = {
+  success: true,
+  transactions: [
+    {
+      height: 664946,
+      tx_hash: 'b7b28a03575bae28c421306fe6727d26c8a6c109b03dfdd276e7bfe32d83e850'
+    },
+    {
+      height: 665022,
+      tx_hash: '01ea002c96b76fb8ac7646aaa70af8684813e3b48baa8de6c68dcb2a6407027c'
+    },
+    {
+      height: 665023,
+      tx_hash: '91bc75f2c39c57e3fb0a6c1a07632220ef5fe04095f68e8a2719665d41fc645f'
+    }
+  ]
+}
+
+const mockParentUtxo1 = {
+  slpUtxos: [
+    {
+      utxos: [
+        {
+          height: 664946,
+          tx_hash: 'b7b28a03575bae28c421306fe6727d26c8a6c109b03dfdd276e7bfe32d83e850',
+          tx_pos: 2,
+          txid: 'b7b28a03575bae28c421306fe6727d26c8a6c109b03dfdd276e7bfe32d83e850',
+          vout: 2,
+          utxoType: 'token',
+          transactionType: 'send',
+          tokenId: '38e97c5d7d3585a2cbf3f9580c82ca33985f9cb0845d4dcce220cb709f9538b0',
+          tokenTicker: 'PSF',
+          tokenName: 'Permissionless Software Foundation',
+          tokenDocumentUrl: 'psfoundation.cash',
+          tokenDocumentHash: '',
+          decimals: 8,
+          tokenType: 1,
+          tokenQty: '72.65766975',
+          isValid: true
+        }
+      ]
+    }
+  ]
+}
+
+const mockParentUtxo2 = {
+  height: 667151,
+  tx_hash: 'a5f3e9a08b0f592040b7538d0bf95b646c9e416bec0f909f81da6518ba32928f',
+  tx_pos: 2,
+  txid: 'a5f3e9a08b0f592040b7538d0bf95b646c9e416bec0f909f81da6518ba32928f',
+  vout: 2,
+  utxoType: 'token',
+  transactionType: 'send',
+  tokenId: '38e97c5d7d3585a2cbf3f9580c82ca33985f9cb0845d4dcce220cb709f9538b0',
+  tokenTicker: 'PSF',
+  tokenName: 'Permissionless Software Foundation',
+  tokenDocumentUrl: 'psfoundation.cash',
+  tokenDocumentHash: '',
+  decimals: 8,
+  tokenType: 1,
+  tokenQty: '100.24446712',
+  isValid: true
+}
+
 module.exports = {
   mockUtxos,
   mockHydratedUtxos,
   mockTokenUtxos,
-  meritHydratedUtxos
+  meritHydratedUtxos,
+  mockTxInputs,
+  mockTxHistory,
+  mockParentUtxo1,
+  mockParentUtxo2
 }
